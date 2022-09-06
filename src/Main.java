6c0ac77aeead0f0c7cf1a08f03e48dba4584b411
sooryasrajan <soorya.s27@gmail.com>
2022-09-06 13:49:03 +0530

    Added data file Restructured project

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.Token;
+
+public class Main {
+
+    public static void main(String[] args) {
+        try {
+            CharStream input = new ANTLRFileStream("/Users/sooryasrajan/AntlrCompilerDesign/src/data");
+            /* give your filepath in the above place*/
+            Lexar lexer = new Lexar(input);
+            String[] ruleNames = lexer.getRuleNames();
+            Token token;
+            while ((token = lexer.nextToken()).getType() != -1) {
+                System.err.println("<" + ruleNames[token.getType() - 1] + ", \"" + token.getText() + "\">");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
