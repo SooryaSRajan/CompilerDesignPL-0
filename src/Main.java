7b835ae8a5350285d70719a54142632dc2755e2b
AdheenaBabu <babu.adheena.2001@gmail.com>
2022-09-16 12:40:23 +0530

    Changed Main

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,3 @@
-import GeneratedOutput.Lexar;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
@@ -8,13 +7,18 @@ public class Main {
     public static void main(String[] args) {
 
         try {
-            CharStream input = new ANTLRFileStream("/Users/sooryasrajan/AntlrCompilerDesign/src/InputFiles/fibonacci");
+            CharStream input = new ANTLRFileStream("/Users/adheenababu/IdeaProjects/CompilerDesignPL-0/src/InputFiles/sum");
+//            CharStream input = new ANTLRFileStream("/Users/adheenababu/IdeaProjects/CompilerDesignPL-0/src/InputFiles/multiplication");
+//            CharStream input = new ANTLRFileStream("/Users/adheenababu/IdeaProjects/CompilerDesignPL-0/src/InputFiles/odd_numbers");
+//            CharStream input = new ANTLRFileStream("/Users/adheenababu/IdeaProjects/CompilerDesignPL-0/src/InputFiles/fibonacci");
+//            CharStream input = new ANTLRFileStream("/Users/adheenababu/IdeaProjects/CompilerDesignPL-0/src/InputFiles/stack");
+
             /* give your filepath in the above place*/
             Lexar lexer = new Lexar(input);
             String[] ruleNames = lexer.getRuleNames();
             Token token;
             while ((token = lexer.nextToken()).getType() != -1) {
-                System.err.println("<" + ruleNames[token.getType() - 1] + ", \"" + token.getText() + "\">");
+                System.err.println("Line: " + token.getLine() + ":" + token.getStartIndex() + ", Token: <" + ruleNames[token.getType() - 1] + ", \"" + token.getText() + "\">");
             }
         } catch (Exception e) {
             System.out.println(e);
