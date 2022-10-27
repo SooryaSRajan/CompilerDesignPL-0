08633ef37da321591a00a5355f9833262e0a2302
sooryasrajan <soorya.s27@gmail.com>
2022-10-27 21:37:04 +0530

    Added main to gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,40 @@
 import GeneratedOutput.LexarLexer;
 import GeneratedOutput.LexarParser;
+import RDPFiles.RDPParserPL0;
 import org.antlr.v4.runtime.ANTLRFileStream;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.Token;
 
 public class Main {
 
     public static void main(String[] args) {
 
         try {
-//            CharStream input = new ANTLRFileStream("/Users/adheenababu/IdeaProjects/CompilerDesignPL-0/src/InputFiles/sum");
-//            CharStream input = new ANTLRFileStream("/Users/adheenababu/IdeaProjects/CompilerDesignPL-0/src/InputFiles/multiplication");
-            CharStream input = new ANTLRFileStream("/Users/sooryasrajan/AntlrCompilerDesign/src/InputFiles/stack");
-//            CharStream input = new ANTLRFileStream("/Users/adheenababu/IdeaProjects/CompilerDesignPL-0/src/InputFiles/fibonacci");
-//            CharStream input = new ANTLRFileStream("/Users/adheenababu/IdeaProjects/CompilerDesignPL-0/src/InputFiles/stack");
 
-            /* give your filepath in the above place*/
+            //gets the file as a character stream
+            //uncomment the file path as needed
+
+            CharStream input = new ANTLRFileStream("/Users/sooryasrajan/AntlrCompilerDesign/src/InputFiles/sum");
+            //CharStream input = new ANTLRFileStream("/Users/sooryasrajan/AntlrCompilerDesign/src/InputFiles/multiplication");
+            //CharStream input = new ANTLRFileStream("/Users/sooryasrajan/AntlrCompilerDesign/src/InputFiles/odd_numbers");
+            //CharStream input = new ANTLRFileStream("/Users/sooryasrajan/AntlrCompilerDesign/src/InputFiles/fibonacci");
+            //CharStream input = new ANTLRFileStream("/Users/sooryasrajan/AntlrCompilerDesign/src/InputFiles/stack");
+
+            //generates a lexer output for the input code
             LexarLexer lexer = new LexarLexer(input);
-            String[] ruleNames = lexer.getRuleNames();
-//            Token token;
-//            while ((token = lexer.nextToken()).getType() != -1) {
-//                System.err.println("Line: " + token.getLine() + ":" + token.getStartIndex() + ", Token: <" + ruleNames[token.getType() - 1] + ", \"" + token.getText() + "\">");
-//            }
+
+            //Gets tokens for the input file
             CommonTokenStream token = new CommonTokenStream(lexer);
+
+            RDPParserPL0 parserPL0 = new RDPParserPL0(token);
+
+            //The token stream is parsed with the respective grammars to check for errors
             LexarParser parser = new LexarParser(token);
             parser.start();
+
+
         } catch (Exception e) {
+            //In case of parse exceptions, an error is thrown and handled here
             System.out.println(e.getLocalizedMessage());
         }
     }
--- src/RDPFiles/RDPParserPL0.java
+++ src/RDPFiles/RDPParserPL0.java
@@ -8,11 +8,10 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import java.io.IOException;
 
 public class RDPParserPL0 {
-    public void start() throws IOException {
 
-        CharStream input = new ANTLRFileStream("/Users/sooryasrajan/AntlrCompilerDesign/src/InputFiles/sum");
-        LexarLexer lexer = new LexarLexer(input);
-        CommonTokenStream token = new CommonTokenStream(lexer);
+    CommonTokenStream tokenStream;
 
+    public RDPParserPL0(CommonTokenStream tokenStream) {
+        this.tokenStream = tokenStream;
     }
 }
