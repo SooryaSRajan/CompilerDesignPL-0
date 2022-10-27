59ebdb583f19cfc30225e4aecac4542adbd2cfd7
sooryasrajan <soorya.s27@gmail.com>
2022-10-27 21:34:32 +0530

    Added main to gitignore

--- src/RDPFiles/RDPParserPL0.java
+++ src/RDPFiles/RDPParserPL0.java
@@ -1,4 +1,18 @@
 package RDPFiles;
 
+import GeneratedOutput.LexarLexer;
+import org.antlr.v4.runtime.ANTLRFileStream;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+
+import java.io.IOException;
+
 public class RDPParserPL0 {
+    public void start() throws IOException {
+
+        CharStream input = new ANTLRFileStream("/Users/sooryasrajan/AntlrCompilerDesign/src/InputFiles/sum");
+        LexarLexer lexer = new LexarLexer(input);
+        CommonTokenStream token = new CommonTokenStream(lexer);
+
+    }
 }
